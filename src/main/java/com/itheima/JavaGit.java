5d316c28388fb19a612932fe98fcb09f795f8f6c
Yuaixin <yu961318207@163.com>
2022-05-10 23:12:58 +0800

    second commit

---
+++ src/main/java/com/itheima/JavaGit.java
@@ -0,0 +1,8 @@
+package com.itheima;
+
+public class JavaGit {
+    public static void main(String[] args) {
+        System.out.println("hello git on idea");
+        System.out.println("hello git 2");
+    }
+}
