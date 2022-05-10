d305fb47ea35f29099cbecdeebaf4ac377a3260e
Yuaixin <yu961318207@163.com>
2022-05-10 23:18:21 +0800

    third commit

--- src/main/java/com/itheima/JavaGit.java
+++ src/main/java/com/itheima/JavaGit.java
@@ -4,5 +4,6 @@ public class JavaGit {
     public static void main(String[] args) {
         System.out.println("hello git on idea");
         System.out.println("hello git 2");
+        System.out.println("hello3");
     }
 }
